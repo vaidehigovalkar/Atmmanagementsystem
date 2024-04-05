73d2c592b200a38b9fad1f622e4f2bccfa720fc5
vaidehigovalkar <164716974+vaidehigovalkar@users.noreply.github.com>
2024-04-05 23:21:42 +0530

    Add files via upload

---
+++ jScrollPane1.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JTable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vaidehi
+ */
+class jScrollPane1 {
+
+    static void setViewportView(JTable jTable1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
